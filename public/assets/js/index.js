07ac04079bf1456104e7c2f30999f16aea0cdc4a
Chris Flick <chrisflick@gmail.com>
2019-10-17 10:06:16 -0700

    created function for deleting an element from db.json

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,74 +1,91 @@
-var $noteTitle = $(".note-title");
-var $noteText = $(".note-textarea");
-var $saveNoteBtn = $(".save-note");
-var $newNoteBtn = $(".new-note");
-var $noteList = $(".list-container .list-group");
+const fs = require("fs")
+const util = require('util')
+const path = require('path')
+
+const writeFileAsync = util.promisify(fs.writeFile)
+
+// var $noteTitle = $(".note-title");
+// var $noteText = $(".note-textarea");
+// var $saveNoteBtn = $(".save-note");
+// var $newNoteBtn = $(".new-note");
+// var $noteList = $(".list-container .list-group");
 
 // activeNote is used to keep track of the note in the textarea
 var activeNote = {};
 
+
 // A function for getting all notes from the db
-var getNotes = function() {
-  
+var getNotes = function () {
+    let db = require("../../../db/db.json")
+    return db;
 };
 
 // A function for saving a note to the db
-var saveNote = function(note) {
-  
+var saveNote = function (note) {
+    let db = getNotes();
+    db.push(activeNote)
+
+    writeFileAsync(path.join(__dirname, '../../../db/db.json'), JSON.stringify(db))
 };
 
 // A function for deleting a note from the db
-var deleteNote = function(title) {
-  
-};
+var deleteNote = function (title) {
+    let db = getNotes()
+    let pos = db.map(function (e) { return e.title; }).indexOf(title)
+    
+    db.splice(pos, 1)
+    writeFileAsync(path.join(__dirname, '../../../db/db.json'), JSON.stringify(db))
+}
 
 // If there is an activeNote, display it, otherwise render empty inputs
-var renderActiveNote = function() {
-  
+var renderActiveNote = function () {
+
 };
 
 // Get the note data from the inputs, save it to the db and update the view
-var handleNoteSave = function() {
-  
+var handleNoteSave = function () {
+
 };
 
 // Delete the clicked note
-var handleNoteDelete = function(event) {
-  
+var handleNoteDelete = function (event) {
+
 };
 
 // Sets the activeNote and displays it
-var handleNoteView = function() {
-  
+var handleNoteView = function () {
+
 };
 
 // Sets the activeNote to and empty object and allows the user to enter a new note
-var handleNewNoteView = function() {
-  
+var handleNewNoteView = function () {
+
 };
 
 // If a note's title or text are empty, hide the save button
 // Or else show it
-var handleRenderSaveBtn = function() {
-  
+var handleRenderSaveBtn = function () {
+
 };
 
 // Render's the list of note titles
-var renderNoteList = function(notes) {
-  
+var renderNoteList = function (notes) {
+
 };
 
 // Gets notes from the db and renders them to the sidebar
-var getAndRenderNotes = function() {
-  
+var getAndRenderNotes = function () {
+
 };
 
-$saveNoteBtn.on("click", handleNoteSave);
-$noteList.on("click", ".list-group-item", handleNoteView);
-$newNoteBtn.on("click", handleNewNoteView);
-$noteList.on("click", ".delete-note", handleNoteDelete);
-$noteTitle.on("keyup", handleRenderSaveBtn);
-$noteText.on("keyup", handleRenderSaveBtn);
+// $saveNoteBtn.on("click", handleNoteSave);
+// $noteList.on("click", ".list-group-item", handleNoteView);
+// $newNoteBtn.on("click", handleNewNoteView);
+// $noteList.on("click", ".delete-note", handleNoteDelete);
+// $noteTitle.on("keyup", handleRenderSaveBtn);
+// $noteText.on("keyup", handleRenderSaveBtn);
 
 // Gets and renders the initial list of notes
-getAndRenderNotes();
+// getAndRenderNotes();
+
+deleteNote('test')
